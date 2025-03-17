b8a8f928740a98a7a59a80e3b1784d5d51eaccd4
marloq1 <yoguar0802@yandex.ru>
2025-03-17 23:03:58 +0300

    Сделал каркас

---
+++ stats/stats-client/src/main/java/ru/practicum/Client.java
@@ -0,0 +1,6 @@
+package ru.practicum;
+
+public class Client {
+    public static void main(String[] args) {
+    }
+}
---
+++ stats/stats-server/src/main/java/ru/practicum/EndPointHit.java
@@ -0,0 +1,15 @@
+package ru.practicum;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+//@Entity
+@Getter
+@Setter
+//@Table(name = "EndpointHit")
+@Builder
+public class EndPointHit {
+}
